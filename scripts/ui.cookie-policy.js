7d87befcf58bf56c8bf06970669d012d3380d3d4
Matt Smith <mattonfoot@gmail.com>
2015-02-05 09:00:32 +0000

    Updates

--- scripts/ui.cookie-policy.js
+++ scripts/ui.cookie-policy.js
@@ -4,7 +4,7 @@
   var key = 'seen_cookie_message';
   var value = 'yes';
   var keyCheck = new RegExp('(?:(?:^|.*;\s*)'+ key +'\s*\=\s*([^;]*).*$)|^.*$');
-  var banner = '<div style="display:block;font-family:Lato,Arial,Helvetica,sans-serif;background:#ddeef4;border:1px solid #cddee4;color:#11779b;" id="cookiePolicy"><div style="display:block;margin:0 auto;width:95.74468085%;max-width:1170px;padding:12px 36px 12px 0;padding:1.2rem 3.6rem 1.2rem 0"><button style="float:right;margin:0 -36px 0 0;margin:0 -3.6rem 0 0;padding:0 1em;line-height:24px;line-height:2.4rem;background:none;border:0;color:#000;cursor:pointer;" onclick="window.ui.removeEPolicyBanner();return false;">×</button>NICE uses cookies to make the site better. <a href="http://www.nice.org.uk/cookies" target="_blank" style="font-weight:700;color:inherit">Learn more</a></div></div>';
+  var banner = '<div style="display:block;font-family:Lato,Arial,Helvetica,sans-serif;background:#ddeef4;border:1px solid #cddee4;color:#11779b;" id="cookiePolicy"><div style="display:block;margin:0 auto;width:95.74468085%;max-width:1170px;padding:12px 36px 12px 0;padding:1.2rem 3.6rem 1.2rem 0"><button style="float:right;margin:0 -36px 0 0;margin:0 -3.6rem 0 0;padding:0 1em;line-height:24px;line-height:2.4rem;background:none;border:0;color:#000;cursor:pointer;" onclick="window.ui.removeEPolicyBanner();return false;">×</button>NICE uses cookies to make the site better. <a href="' + url + '" target="_blank" style="font-weight:700;color:inherit;display:inline">Learn more</a></div></div>';
 
   if ( document.cookie.replace( keyCheck, '$1' ) !== value ) {
     setTimeout(cookiePolicyDisplay, 50);
@@ -23,7 +23,7 @@
 
     var container = document.createElement('div');
     container.innerHTML = banner;
-    document.body.insertBefore( container.firstChild, tophat.nextSibling );
+    tophat.appendChild( container.firstChild );
   }
 
   function locateTophat() {
