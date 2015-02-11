06fc1bf63fa8e66e1d9c9d4b75ff00294fdb1c40
Matt Smith <mattonfoot@gmail.com>
2015-02-11 16:18:12 +0000

    added error page prototypes

--- src/scripts/ui.eprivacy-policy.js
+++ src/scripts/ui.eprivacy-policy.js
@@ -5,7 +5,7 @@
   var value = 'yes';
   var banner = '<div style="display:block;font-family:Lato,Arial,Helvetica,sans-serif;background:#ddeef4;border:1px solid #cddee4;color:#11779b;" id="cookiePolicy"><div style="display:block;margin:0 auto;width:95.74468085%;max-width:1170px;padding:12px 36px 12px 0"><button style="float:right;margin:0 -36px 0 0;padding:0 1em;line-height:24px;line-height:24px;background:none;border:0;color:#000;cursor:pointer;" onclick="window.ui.removeEPolicyBanner();return false;">×</button>NICE uses cookies to make the site better. <a href="'+ url +'" target="_blank" style="font-weight:700;color:inherit;display:inline">Learn more</a></div></div>';
 
-  if ( ~document.cookie.indexOf( key + '=' + value ) ) {
+  if ( !~document.cookie.indexOf( key + '=' + value ) ) {
     setTimeout(cookiePolicyDisplay, 50);
   }
 
